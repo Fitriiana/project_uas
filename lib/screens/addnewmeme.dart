b9246d6629ceeed24fcaaa035230350912012755
dhikananda <s160419063@student.ubaya.ac.id>
2022-12-14 16:04:05 +0700

    addnewmeme otw

--- lib/screens/addnewmeme.dart
+++ lib/screens/addnewmeme.dart
@@ -1,15 +1,9 @@
+import 'dart:io';
+import 'package:image/image.dart' as img;
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 
 class AddNewMeme extends StatefulWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: const Center(
-        child: Text('This is the history screen'),
-      ),
-    );
-  }
-
   @override
   State<StatefulWidget> createState() {
     return _AddNewMemeState();
@@ -17,10 +11,34 @@ class AddNewMeme extends StatefulWidget {
 }
 
 class _AddNewMemeState extends State<AddNewMeme> {
+  TextEditingController _url = TextEditingController();
+  TextEditingController _teksAtas = TextEditingController();
+  TextEditingController _teksBawah = TextEditingController();
+
+  File? _image;
+  File? _imageProses;
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Create Your Meme'),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.all(8),
+          child: Column(
+            children: [
+              Text('Preview'),
+              Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Image.network("https://ubaya.fun/blank.jpg")
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 
 }
